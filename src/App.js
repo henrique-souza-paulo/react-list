701204bd63583dc56390d639bbda43e1e3fa51b9
Paulo Souza <paulo18santos2000@gmail.com>
2023-01-21 02:03:05 -0300

    alteracao rota home

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     // Rotas
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
         <Route path='primeiralista' element={<Listaum />} />
         <Route path='segundalista' element={<Listadois />} />
         <Route path='terceiralista' element={<Listatres />} />
